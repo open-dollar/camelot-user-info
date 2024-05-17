9d1c10d1b635223956e6eb92ff85c576472bb728
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-16 18:21:43 -0700

    Working value from spNFT and NitroPool

--- camelot.js
+++ camelot.js
@@ -16,7 +16,34 @@ const fromBigNumber = (number, decimals = 18) => {
   return parseFloat(formatUnits(number.toString(), decimals))
 }
 
-const lpBalanceFromSpNFTs = async (userAddress, spNFTAddress, collateral0Address, collateral1Address) => {
+const valueFromAlgebraPositions = async (userAddress, collateral0Address, collateral1Address) => {
+  const CAMELOT_V1_NFT_ADDRESS = "0x00c7f3082833e796A5b3e4Bd59f6642FF44DCD15"
+  const algebraPositionsContract = getContract(CAMELOT_V1_NFT_ADDRESS, AlgebraPositions.abi)
+  const nftCount = await algebraPositionsContract.balanceOf(userAddress);
+  let lpBalance = 0;
+  for (let i = 0; i < nftCount; i++) {
+    const tokenId = await algebraPositionsContract.tokenOfOwnerByIndex(userAddress, i);
+    const positions = await algebraPositionsContract.positions(tokenId);
+    if (positions.token0.toLowerCase() === collateral0Address.toLowerCase() && positions.token1.toLowerCase() === collateral1Address.toLowerCase()) {
+      lpBalance += fromBigNumber(positions.liquidity);
+    }
+  }
+
+  // Convert LP Balance to dollar value
+
+  // Calculate the user's share
+  const userDollarValue = 0
+
+  console.log('Value from Algebra Positions V1:', userDollarValue);
+  return userDollarValue;
+}
+
+const valueFromSpNFTs = async (userAddress, nitroPoolAddress, collateral0Address, collateral1Address) => {
+  let response = await fetch('https://api.camelot.exchange/nitros/')
+  let res = await response.json()
+  const nitroData = res.data.nitros[nitroPoolAddress]
+
+  spNFTAddress = nitroData.nftPool
   const spNFTContract = getContract(spNFTAddress, SpNFT.abi);
 
   // Validate LP collateral tokens match expected values
@@ -37,103 +64,59 @@ const lpBalanceFromSpNFTs = async (userAddress, spNFTAddress, collateral0Address
     const positionDetails = await spNFTContract.getStakingPosition(tokenId);
     lpBalance += fromBigNumber(positionDetails.amount);
   }
-  console.log('LP Balance from spNFT:', lpBalance)
-  return lpBalance
+
+  // Convert LP Balance to dollar value
+  response = await fetch('https://api.camelot.exchange/nft-pools/')
+  res = await response.json()
+
+  let nftPools = Object.values(res.data.nftPools)
+  let relevantNftPool = nftPools.find(pool => pool.address === spNFTAddress)
+
+  // Calculate the user's share
+  const userShare = lpBalance / fromBigNumber(relevantNftPool.totalDeposit);
+  const userDollarValue = userShare * relevantNftPool.tvlUSD
+
+  console.log('Value from SpNFTs:', userDollarValue);
+  return userDollarValue
 }
 
-const lpBalanceFromAlgebraPositions = async (userAddress, collateral0Address, collateral1Address) => {
-  const CAMELOT_V1_NFT_ADDRESS = "0x00c7f3082833e796A5b3e4Bd59f6642FF44DCD15"
-  const algebraPositionsContract = getContract(CAMELOT_V1_NFT_ADDRESS, AlgebraPositions.abi)
-  const nftCount = await algebraPositionsContract.balanceOf(userAddress);
-  let lpBalance = 0;
-  for (let i = 0; i < nftCount; i++) {
-    const tokenId = await algebraPositionsContract.tokenOfOwnerByIndex(userAddress, i);
-    const positions = await algebraPositionsContract.positions(tokenId);
-    if (positions.token0.toLowerCase() === collateral0Address.toLowerCase() && positions.token1.toLowerCase() === collateral1Address.toLowerCase()) {
-      lpBalance += fromBigNumber(positions.liquidity);
-    }
-  }
-  console.log('LP Balance from Algebra Positions V1:', lpBalance);
-  return lpBalance;
-}
 
-const fetchUserPoolDetails = async (poolAddress, nitroPoolAddress, userAddress) => {
-  // Fetch both pools
+
+const valueFromNitroPool = async (userAddress, nitroPoolAddress, collateral0Address, collateral1Address) => {
   const camelotNitroPool = getContract(nitroPoolAddress, CamelotNitroPool.abi)
-  const camelotPool = getContract(poolAddress, CamelotPool.abi)
-
-  // Fetch collateral tokens for pools
-  const collateral0Address = await camelotPool.token0()  // OD
-  const collateral1Address = await camelotPool.token1()  // WETH
-  const collateral0 = getContract(collateral0Address, ERC20.abi)
-  const collateral1 = getContract(collateral1Address, ERC20.abi)
-
-  // Fetch TVL from Camelot's API
-  const response = await fetch('https://api.camelot.exchange/nitros')
-  const res = await response.json()
-
-  // Fetch the user's balance in both tokens including spNFT balances
-  const spNFTAddresses = [
-    '0x7647Da336cF43F894aC7A0bf87f04806b2E03bb8',  // OD-ETH spNFT
-  ]
-
-  let lpBalance = 0;
-
-  lpBalance += await lpBalanceFromAlgebraPositions(userAddress, collateral0Address, collateral1Address);
-
-  for (const spNFTAddress of spNFTAddresses) {
-    lpBalance += await lpBalanceFromSpNFTs(userAddress, spNFTAddress, collateral0Address, collateral1Address);
-  }
-
-  console.log('Total LP Balance:', lpBalance);
-  return
-  const collateralTokens = [
-    {
-      symbol: await collateral0.symbol(),
-      address: collateral0Address,
-      spNFTBalance: totalCollateral0Balance,
-      userBalance: fromBigNumber(await collateral0.balanceOf(userAddress)),
-      nitroPoolBalance: fromBigNumber(await collateral0.balanceOf(nitroPoolAddress)),
-      poolBalance: fromBigNumber(await collateral0.balanceOf(poolAddress)),
-    },
-    {
-      symbol: await collateral1.symbol(),
-      address: collateral1Address,
-      spNFTBalance: totalCollateral1Balance,
-      userBalance: fromBigNumber(await collateral1.balanceOf(userAddress)),
-      nitroPoolBalance: fromBigNumber(await collateral1.balanceOf(nitroPoolAddress)),
-      poolBalance: fromBigNumber(await collateral1.balanceOf(poolAddress)),
-    }
-  ]
 
   const userInfo = await camelotNitroPool.userInfo(userAddress);
-  const userDepositAmount = fromBigNumber(userInfo.totalDepositAmount);
+
   const totalDepositAmount = fromBigNumber(await camelotNitroPool.totalDepositAmount());
+  const userDepositAmount = fromBigNumber(userInfo.totalDepositAmount);
 
-  // Calculate the user's percentage of the pool
+  // Convert LP Balance to dollar value
+  const response = await fetch('https://api.camelot.exchange/nitros/')
+  const res = await response.json()
+  const nitroData = res.data.nitros[nitroPoolAddress]
+
   const userPoolPercentage = userDepositAmount / totalDepositAmount;
+  const userDollarValue = userPoolPercentage * nitroData.tvlUSD;
 
-  // Calculate the user's share of each collateral token
-  const userCollateralBalances = collateralTokens.map(token => {
-    const totalBalance = token.nitroPoolBalance + token.poolBalance + token.spNFTBalance;
-    const userShare = userPoolPercentage * totalBalance;
-    return {
-      symbol: token.symbol,
-      userShare: userShare
-    };
-  });
-
-  // Calculate the dollar value of the user's share of the pool
-  // const userDollarValue = userPoolPercentage * tvlUSD;
-
-  console.log('Collateral:', collateralTokens);
-  // console.log('User Info:', userInfo);
-  console.log('User Deposit Amount:', userDepositAmount);
-  console.log('Total Deposit Amount:', totalDepositAmount);
-  // console.log('User Pool Percentage:', (userPoolPercentage * 100).toFixed(2) + '%');
-  // console.log('User Dollar Value:', '$' + userDollarValue.toFixed(2));
-  // console.log('TVL USD:', '$' + tvlUSD.toFixed(2));
-  console.log('User Collateral Balances:', userCollateralBalances);
+  console.log('Value from Nitro Pool:', userDollarValue);
+  return userDollarValue
 }
 
-fetchUserPoolDetails('0x824959a55907d5350e73e151Ff48DabC5A37a657', '0x53F973256F410d1D8b10ce72D03D8dBBD3b1066E', '0x9492510BbCB93B6992d8b7Bb67888558E12DCac4')
+const valueInCamelot = async (userAddress, camelotPoolAddress, nitroPoolAddress) => {
+  const camelotPool = getContract(camelotPoolAddress, CamelotPool.abi)
+
+  const collateral0Address = await camelotPool.token0()
+  const collateral1Address = await camelotPool.token1()
+
+  let userDollarValue = 0;
+
+  // Avoid using Algebra Positions V1, since liquidity can be added outside of the current range
+  // userDollarValue += await valueFromAlgebraPositions(userAddress, collateral0Address, collateral1Address);
+
+  userDollarValue += await valueFromSpNFTs(userAddress, nitroPoolAddress, collateral0Address, collateral1Address);
+
+  userDollarValue += await valueFromNitroPool(userAddress, nitroPoolAddress);
+
+  console.log('Total User Deposit: $', userDollarValue);
+  return userDollarValue
+}
